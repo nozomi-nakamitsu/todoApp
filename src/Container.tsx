6fd5feb0997f97cecdfc0b9c9009081d43d8e05c
nozomi-nakamitsu <nozomi_nakamitsu@arsaga.jp>
2021-05-05 13:10:41 +0900

    アラートを表示

--- src/Container.tsx
+++ src/Container.tsx
@@ -1,12 +1,26 @@
-import React,{ FC, useState, useCallback,useContext } from 'react'
+import React,{ FC, useState, useCallback,useEffect} from 'react'
 import { Card } from './Card'
 import update from 'immutability-helper'
 
 import AddIcon from '@material-ui/icons/Add';
 import CheckBoxOutlinedIcon from '@material-ui/icons/CheckBoxOutlined';
 import CheckBoxOutlineBlankOutlinedIcon from '@material-ui/icons/CheckBoxOutlineBlankOutlined';
-
-
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Alert from '@material-ui/lab/Alert';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import Collapse from '@material-ui/core/Collapse';
+// アラート
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+      '& > * + *': {
+        marginTop: theme.spacing(2),
+      },
+    },
+  }),
+);
 export interface Item {
   id: number
   text: string
@@ -20,19 +34,29 @@ export const Container: FC = () => {
   {
     const [task, setTask] = useState("");
     const [cards, setCards] = useState([{ id: 0, name: "" },]);
+    const [message, setMessage] = useState("")
+    const classes = useStyles();
+    const [alert, setAlert] = useState("");
     // インプットタグ入力フォーム値取得
     const handleChange = (event:React.ChangeEvent<HTMLInputElement>) => {
+      const value = event.target.value;
       setTask(() => event.target.value)
-      console.log(task)
     }
     // タスク追加
     const addtask=()=>{
-      var TaskIdArray=cards.map(v=>v.id)
-      var lastTaskID=TaskIdArray.slice(-1)[0]
-      setCards([...cards,{id: lastTaskID+1, name: task}]);
-      setTask("")
+      if (task!=""){
+        setAlert("success")
+        var TaskIdArray=cards.map(v=>v.id)
+        var lastTaskID=TaskIdArray.slice(-1)[0]
+        setCards([...cards,{id: lastTaskID+1, name: task}]);
+        setTask("")
+        setMessage("タスクを追加しました")
+      }else{
+        setAlert("error")
+        setMessage("空欄のタスクは登録できません！")
+      }
+     
     }
-
 
     // エンタキー押した時にタスク追加
     const keyPress=(e :any)=>{
@@ -81,12 +105,49 @@ export const Container: FC = () => {
     <div className="headerItem" onClick={()=> DeleteAllTask()}>全削除</div>
   </div>
   <hr/>
+  <div className={classes.root}>
+{
+alert=="error"?  <Alert severity="error"
+action={
+  <IconButton
+    aria-label="close"
+    color="inherit"
+    size="small"
+    onClick={() => {
+      setAlert("");
+    }}
+  >
+    <CloseIcon fontSize="inherit" />
+  </IconButton>
+}>
+  {message}
+</Alert>:null
+}
+{
+alert=="success"?  <Alert severity="success"
+action={
+  <IconButton
+    aria-label="close"
+    color="inherit"
+    size="small"
+    onClick={() => {
+      setAlert("");
+    }}
+  >
+    <CloseIcon fontSize="inherit" />
+  </IconButton>
+}>
+  {message}
+</Alert>:null
+}
+    </div>
    <CardsContext.Provider value={{cards,setCards}}>
     {cards.length!=1?
     <div>{cards.map((card:any , i:number) => renderCard(card, i))} </div>
     :null} 
     {/* inputタグ */}
     <div className="Input">
+
       < CheckBoxOutlineBlankOutlinedIcon />
         <div className="cp_iptxt">
         <label className="ef">
